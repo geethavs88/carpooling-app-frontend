a5fca4cdae504c79b1ec3351b74515362d84f7d7
geethavs88 <geethavs88@gmail.com>
2026-01-22 09:31:27 -0800

    added button component

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 function PrimaryButton({ children }) {
+    function pressHandler() {
+        console.log("Pressed!");
+    }
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable 
+            style={styles.buttonInnerContainer} 
+            onPress={pressHandler} 
+            android_ripple={{ color: '#0b93b1ff' }}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     );
 }
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        margin: 4,
+        borderRadius: 28,
+        overflow: 'hidden'
+
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#4a98c8ff',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 8
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    }
+});
 export default PrimaryButton;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,32 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 function HomeScreen() {
     return (
-    <View>
-        <Text> Home Screen</Text>
-        <View>
-            <PrimaryButton>Schedule Ride</PrimaryButton>
-            <PrimaryButton>Schedule Drive</PrimaryButton>
-            <PrimaryButton>My Commutes</PrimaryButton>
-            <PrimaryButton>My Profile</PrimaryButton>
-        </View>
-
-    </View>);
+    <View style={styles.homeInputContainer}>
+        <PrimaryButton>Schedule Ride</PrimaryButton>
+        <PrimaryButton>Schedule Drive</PrimaryButton>
+        <PrimaryButton>My Commutes</PrimaryButton>
+        <PrimaryButton>My Profile</PrimaryButton>
+    </View>
+    );
     
 }
+const styles = StyleSheet.create({
+    homeInputContainer: {
+
+        marginTop:100,
+        marginHorizontal: 16,
+        padding: 16,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 8
+
+    }
+
+
+});
 export default HomeScreen;
