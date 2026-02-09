dd4bfad7f1f51ee2872cc1ca60d5303ed433ae32
geethavs88 <geethavs88@gmail.com>
2026-02-08 17:15:05 -0800

    added import statements to profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,10 @@
 import { View, Text, StyleSheet } from 'react-native';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 function ProfileScreen() {
+    const { user } = useContext(AuthContext);
+
     return (
         <View>
             <Text> My Profile </Text>
