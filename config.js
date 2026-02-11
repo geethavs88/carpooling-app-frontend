48ffb638d61b079a3d0236ee05b922efbbf22692
geethavs88 <geethavs88@gmail.com>
2026-02-11 11:56:32 -0800

    deleted api key

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://carpooling-backend-application.onrender.com/api';
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyCPCvtE_jBDzJl3XPPoJrnT4hOR_RqkTUI';
+export const GOOGLE_MAPS_API_KEY = 'xxxx';
